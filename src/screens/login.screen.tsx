fda0fb4a98028e186bf831ab5fed8ca8b86d9790
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-10-27 18:59:38 +0700

    create login screen ui

--- src/screens/login.screen.tsx
+++ src/screens/login.screen.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Button} from '../components/atoms/button';
+import {Gap} from '../components/atoms/gap';
+import {TextInput} from '../components/molecules/text-input';
 
-export default function Login() {
+const Login = () => {
+  function onLogin() {
+    console.log('Login');
+  }
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>Login</Text>
+      <TextInput
+        label="Username"
+        placeholder="Enter your e-mail"
+        containerStyle={styles.margin}
+      />
+      <TextInput
+        label="Password"
+        placeholder="Enter your password"
+        secureTextEntry
+        containerStyle={styles.margin}
+      />
+      <Gap height={16} />
+      <Button onPress={onLogin} title="LOGIN" buttonStyle={styles.margin} />
     </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({});
+export {Login};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#070d2d',
+    justifyContent: 'center',
+  },
+  margin: {
+    marginHorizontal: 8,
+  },
+});
