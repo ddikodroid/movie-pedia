71c7c11129e393e8459ead5c50a1cbec47511535
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-10-30 09:16:30 +0700

    add detail section for detail screen

--- src/screens/detail.screen.tsx
+++ src/screens/detail.screen.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Linking, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {Header} from '../components/molecules/header';
+import {colors} from '../styles/colors';
+import {Button} from '../components/atoms/button';
+import {DetailImage} from '../components/atoms/detail-image';
+import {SectionTitle} from '../components/atoms/section-title';
+import {normalize} from '../utils/normalize';
 
 export type IDetailScreenProps = {
   navigation: any;
@@ -9,10 +14,33 @@ export type IDetailScreenProps = {
 
 const Detail: React.FC<IDetailScreenProps> = ({navigation, route}) => {
   const {item} = route.params;
+  const strippedDesc = item.detail.description.substring(1);
+  console.log(item.detail.description);
   return (
     <SafeAreaView style={styles.container}>
-      <Header title={item.title} />
-      <Text style={styles.white}>{item.movieId}</Text>
+      <Header
+        title={item.title}
+        leftContent={{
+          icon: 'arrow-back',
+          onPress: () => navigation.goBack(),
+        }}
+      />
+      <DetailImage uri={item.detail.thumbnailLandscape} />
+      {item.detail.trailer !== null ? (
+        <Button
+          title="Play trailer video"
+          onPress={() => Linking.openURL(item.detail.trailer)}
+          buttonStyle={styles.playTrailerButton}
+        />
+      ) : null}
+      <View style={styles.margin}>
+        <SectionTitle title="Storyline" style={styles.detailSectionTitle} />
+        <Text style={styles.infoText}>{strippedDesc}</Text>
+        <SectionTitle title="Director" style={styles.detailSectionTitle} />
+        <Text style={styles.infoText}>{item.detail.director}</Text>
+        <SectionTitle title="Actors" style={styles.detailSectionTitle} />
+        <Text style={styles.infoText}>{item.detail.actors}</Text>
+      </View>
     </SafeAreaView>
   );
 };
@@ -22,12 +50,21 @@ export {Detail};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#070d2d',
+    backgroundColor: colors.primary,
   },
   margin: {
-    marginHorizontal: 8,
+    marginHorizontal: normalize(16),
   },
-  white: {
-    color: '#fff',
+  infoText: {
+    color: colors.white,
+    fontSize: normalize(14),
+  },
+  playTrailerButton: {
+    backgroundColor: colors.primary,
+  },
+  detailSectionTitle: {
+    fontSize: normalize(18),
+    marginVertical: normalize(8),
+    color: colors.lightGrey,
   },
 });
