78c6703c9470f8292ce6be2b9416c05d82ac11f2
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-10-31 11:51:47 +0700

    set root view backgroud color using primary color

--- App.tsx
+++ App.tsx
@@ -8,12 +8,17 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppRoute from './src/routes/app.route';
 import {StatusBar} from 'react-native';
+import {setRootViewBackgroundColor} from '@pnthach95/react-native-root-view-background';
+import {colors} from './src/styles/colors';
 
 export default function App() {
+  useEffect(() => {
+    setRootViewBackgroundColor(colors.primary);
+  }, []);
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" />
